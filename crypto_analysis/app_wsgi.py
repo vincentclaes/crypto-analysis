fd95f7d2d68d2bacecab7b72a1bc7ea7a22e2f03
vincent <vclaes1986@gmail.com>
2018-05-17 20:52:08 +0200

    remove double files

--- crypto_analysis/app_wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI entrypoint for gunicorn et al."""
-
-from app import app
-from gevent.wsgi import WSGIServer
-http_server = WSGIServer(('', 80), app)
-http_server.serve_forever()
--- crypto_analysis/endpoints_wsgi.py
+++
@@ -1,6 +0,0 @@
-"""WSGI entrypoint for gunicorn et al."""
-
-from endpoints import app
-from gevent.wsgi import WSGIServer
-http_server = WSGIServer(('', 5004), app)
-http_server.serve_forever()
