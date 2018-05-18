5a5db5c4eb85757d2ea798d32a83fdb9d0025f29
vincent <vclaes1986@gmail.com>
2018-05-18 22:07:51 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='crypto-analysis',
+      version='1.0',
+      description='Crypto analysis',
+      author='vincent claes',
+      author_email='vclaes1986@gmail.com',
+     )
