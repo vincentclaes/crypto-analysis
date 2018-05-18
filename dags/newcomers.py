8b71a8e54d153826b3ed4035c0d4a0a030b8fd9a
vincent <vclaes1986@gmail.com>
2018-05-18 22:01:23 +0200

    add dag for newcomers

--- dags/newcomers.py
+++ dags/newcomers.py
@@ -22,6 +22,12 @@ get_coinmarketcap_data = BashOperator(
 
 create_newcomers_top100 = BashOperator(
     task_id='create_newcomers_top100',
-    bash_command='python /home/ec2-user/projects/coinmarketcap_data/coinmarketcap_data.py',
+    bash_command='python /home/ec2-user/projects/crypto-analysis/scripts/get_newcomers.py 100 100',
     dag=dag)
-create_newcomers_top200
+
+create_newcomers_top200 = BashOperator(
+    task_id='create_newcomers_top200',
+    bash_command='python /home/ec2-user/projects/crypto-analysis/scripts/get_newcomers.py 200 100',
+    dag=dag)
+
+get_coinmarketcap_data >> create_newcomers_top100 >> create_newcomers_top200
