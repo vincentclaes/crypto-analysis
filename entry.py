656bccfdcd60b91976a922cf22b3c46da88f74ef
vincent <vclaes1986@gmail.com>
2018-06-16 11:26:01 +0200

    preliminary code and call of cli

--- entry.py
+++ entry.py
@@ -5,13 +5,33 @@ from crypto_analysis.controllers import newcomers
 from crypto_analysis.databases import Connection
 from crypto_analysis.databases import DB
 
+
+def get_newcomers(args):
+    newcomers.get_newcomers(conn, args.rank, args.no, args.latest)
+
+
+def tweet(args):
+    pass
+
+
 if __name__ == '__main__':
     conn = Connection.get_connection(DB)
     logging.basicConfig(level=logging.DEBUG)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--rank", help="rank you want to use", type=int, required=True)
-    parser.add_argument("-n", "--no", help="number of results you want", type=int, required=True)
-    parser.add_argument("--latest", help="do i only want to get the latest newcomers", action="store_true")
+    subparsers = parser.add_subparsers(help='extra functions for newcomers, notifications etc.')
+
+    get_newcomers_parser = subparsers.add_parser("newcomers")
+    get_newcomers_parser.add_argument("-r", "--rank", help="rank you want to use", type=int, required=True)
+    get_newcomers_parser.add_argument("-n", "--no", help="number of results you want", type=int, required=True)
+    get_newcomers_parser.add_argument("--latest", help="do i only want to get the latest newcomers",
+                                      action="store_true")
+    get_newcomers_parser.set_defaults(func=get_newcomers)
+
+    get_newcomers_parser = subparsers.add_parser("tweet")
+    get_newcomers_parser.add_argument("-r", "--rank", help="rank you want to use", type=int, required=True)
+    get_newcomers_parser.add_argument("-i", "--ids", help="ids you want to tweet", nargs='+', required=True)
+    get_newcomers_parser.set_defaults(func=tweet)
+
     args = parser.parse_args()
-    newcomers.get_newcomers(conn, args.rank, args.no, args.latest)
+    args.func(args)
